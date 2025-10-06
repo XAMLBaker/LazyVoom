47853183edbd47d5a4eeed06550f49ec4f597517
lukewire <lukewire129@gmail.com>
2025-10-07 00:21:24 +0900

    - WinUI3 버전 출시

---
+++ src/LazyVoom.WinUI3/ViewModelLocator.cs
@@ -0,0 +1,45 @@
+using LazyVoom.Core;
+using Microsoft.UI.Xaml;
+using System;
+using System.Diagnostics;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace LazyVoom.WinUI3;
+
+public static class ViewModelLocator
+{
+    public static readonly DependencyProperty AutoWireViewModelProperty =
+        DependencyProperty.RegisterAttached ("AutoWireViewModel", typeof (bool), typeof (ViewModelLocator),
+            new PropertyMetadata (false, OnAutoWireViewModelChanged));
+
+    public static bool GetAutoWireViewModel(DependencyObject obj)
+    {
+        return (bool)obj.GetValue (AutoWireViewModelProperty);
+    }
+
+    public static void SetAutoWireViewModel(DependencyObject obj, bool value)
+    {
+        obj.SetValue (AutoWireViewModelProperty, value);
+    }
+
+    private static void OnAutoWireViewModelChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var value = (bool?)e.NewValue;
+        if (value.HasValue && value.Value)
+        {
+            Debug.WriteLine ($"[{0}] [Locator] {d.GetType ().Name} ViewModelLocator에 의해 자동 연결처리 합니다.", DateTime.Now.ToString ());
+            d.AutoBindViewModel (Bind);
+        }
+    }
+
+    static void Bind(object view, object viewModel)
+    {
+        if (view is FrameworkElement element)
+        {
+            Debug.WriteLine ($"[{0}] [Locator] {view.GetType ().Name}과 {viewModel.GetType ().Name}과 연결되었습니다.", DateTime.Now.ToString ());
+            element.DataContext = viewModel;
+        }
+    }
+}
