26ad289c0ecabe3621ac34370ce6ce1e05d1a8be
lukewire <lukewire129@gmail.com>
2025-10-10 07:32:01 +0900

    WinUI3 샘플 코드 추가

---
+++ src/Sample/WinUI3/1. Base/Sample1/MainPageViewModel.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Sample1
+{
+    public partial class MainPageViewModel : ObservableObject
+    {
+        [ObservableProperty] int count;
+
+        [RelayCommand]
+        private void Increment()
+        {
+            Count++;
+        }
+
+        [RelayCommand]
+        private void Decrement()
+        {
+            Count--;
+        }
+    }
+}
