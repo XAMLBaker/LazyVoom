f9c2476773cca8f34111e2112e5eaa078f0c1a64
lukewire <lukewire129@gmail.com>
2025-10-10 06:45:14 +0900

    Avalonia Sample 추가

---
+++ src/Sample/Avalonia/1. Base/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Sample1.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
---
+++ src/Sample/Avalonia/1. Base/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Sample1.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}
